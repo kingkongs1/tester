30c7fb656b13e53f7e50c4744a6a9813aaa22eb5
ambler082 <zzzodg@gmail.com>
2020-11-24 01:51:54 +0900

    해파리

---
+++ catalog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import sqlite3
+
+
+# Create your models here.
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=10)
+    introduction = models.TextField()
+    area = models.CharField(max_length=15)
+    party_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
---
+++ catalog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Candidate
+
+# Create your views here.
+
+def index(request):  #어떤url요청에대해서 index가 실행되는지 조건을 설정해줘야함
+    candidates = Candidate.objects.all()
+    context = {'candidates' : candidates}
+    return render(request, 'catalog/index.html', context)
