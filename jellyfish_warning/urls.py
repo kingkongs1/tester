9b84e9d25225973c2ae64bebddc33b38cc21b434
ambler082 <zzzodg@gmail.com>
2020-11-26 15:58:19 +0900

    asd

--- jellyfish_warning/urls.py
+++ jellyfish_warning/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
-    path('ec2-3-35-136-93.ap-northeast-2.compute.amazonaws.com ', include('catalog.urls')),
+    path('ec2-3-35-136-93.ap-northeast-2.compute.amazonaws.com', include('catalog.urls')),
 ]
